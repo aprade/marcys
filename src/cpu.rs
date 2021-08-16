f727a848d0953ae2b091758db990309cb5428ca6
Joao Koritar <jakoritarleite@gmail.com>
2021-08-16 20:51:22 -0300

    feat: implement the first route that gets cpu info

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,20 +1,22 @@
+use rocket::serde::Serialize;
 use sys_metrics::cpu::*;
 
-#[derive(Debug)]
-struct CPUInfo {
-    frequency: f64,
-    logical_cores: u32,
-    physical_cores: u32,
-    load_average: LoadAvg,
+#[derive(Debug, Serialize)]
+pub struct CPUInfo {
+    pub frequency: f64,
+    pub logical_cores: u32,
+    pub physical_cores: u32,
+    pub load_average: LoadAvg,
 }
 
-pub fn get_cpu_info() {
+pub fn get_cpu_info() -> CPUInfo {
     let info = CPUInfo {
         frequency: get_cpufreq().unwrap(),
         logical_cores: get_logical_count().unwrap(),
         physical_cores: get_physical_count().unwrap(),
         load_average: get_loadavg().unwrap(),
     };
-
     println!("{:#?}", info);
+
+    info
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+#[macro_use] extern crate rocket;
+
 mod cpu;
 
-fn main() {
-    cpu::get_cpu_info();
+use rocket::serde::{json::Json};
+
+#[get("/cpu")]
+fn route_cpu() -> Json<cpu::CPUInfo> {
+    let info = cpu::get_cpu_info();
+    Json(info);
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![route_cpu])
+}
+
+/*fn main() {
+    cpu::get_cpu_info();
+}*/
